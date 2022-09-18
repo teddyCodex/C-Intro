49362ce25b91fd9f1928e637f7c8bc5e6ad4f87c
Samted <owner@Teddys-Mac.local>
2022-09-19 00:31:18 +0100

    update c file

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-main()
+int main()
 {
-    printf("Hello World!\n");
-    return 0;
+    char CharacterName[] = "John";
+    int CharacterAge = 40;
+
+    printf("%s is a middle-aged man from New York. \n", CharacterName);
+    printf("\n");
+    printf("He just turned %d and was hopeful. \n", CharacterAge);
+    printf("\n");
+    printf("As they say, life begins at %d. \n", CharacterAge);
 }
